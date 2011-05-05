709c5acbe1c0f96e477e7bc800d327fa6f5b96df
Andy Yanok <ayanok@me.com>
2011-05-05 07:59:45 -0400

    Added

---
+++ AYUIButton.h
@@ -0,0 +1,29 @@
+//
+//  AYUIButton.h
+//  etsy-for-ipad
+//
+//  Created by Andy Yanok on 5/5/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <QuartzCore/QuartzCore.h>
+
+/*
+    This subclass was created to be able to set the background color for UIControlState
+ 
+ 
+*/
+
+@interface AYUIButton : UIButton 
+{
+@private
+    NSMutableDictionary *backgroundStates;
+@public
+    
+}
+
+- (void) setBackgroundColor:(UIColor *) _backgroundColor forState:(UIControlState) _state;
+- (UIColor*) backgroundColorForState:(UIControlState) _state;
+
+@end
---
+++ AYUIButton.m
@@ -0,0 +1,75 @@
+//
+//  AYUIButton.m
+//  etsy-for-ipad
+//
+//  Created by Andy Yanok on 5/5/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AYUIButton.h"
+
+
+@implementation AYUIButton
+
+- (void) setBackgroundColor:(UIColor *) _backgroundColor forState:(UIControlState) _state {
+    if (backgroundStates == nil) 
+        backgroundStates = [[NSMutableDictionary alloc] init];
+    
+    [backgroundStates setObject:_backgroundColor forKey:[NSNumber numberWithInt:_state]];
+    
+    if (self.backgroundColor == nil)
+        [self setBackgroundColor:_backgroundColor];
+}
+
+- (UIColor*) backgroundColorForState:(UIControlState) _state {
+    return [backgroundStates objectForKey:[NSNumber numberWithInt:_state]];
+}
+
+#pragma mark -
+#pragma mark Touches
+
+- (void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+	[super touchesBegan:touches withEvent:event];
+    
+    UIColor *selectedColor = [backgroundStates objectForKey:[NSNumber numberWithInt:UIControlStateHighlighted]];
+    if (selectedColor) {
+        CATransition *animation = [CATransition animation];
+        [animation setType:kCATransitionFade];
+        [animation setTimingFunction:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut]];
+        [self.layer addAnimation:animation forKey:@"EaseOut"];
+        self.backgroundColor = selectedColor;
+    }
+}
+
+- (void) touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event {
+	[super touchesCancelled:touches withEvent:event];
+
+    UIColor *normalColor = [backgroundStates objectForKey:[NSNumber numberWithInt:UIControlStateNormal]];
+    if (normalColor) {
+        CATransition *animation = [CATransition animation];
+        [animation setType:kCATransitionFade];
+        [animation setTimingFunction:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut]];
+        [self.layer addAnimation:animation forKey:@"EaseOut"];
+        self.backgroundColor = normalColor;
+    }
+}
+
+- (void) touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    [super touchesEnded:touches withEvent:event];
+    
+    UIColor *normalColor = [backgroundStates objectForKey:[NSNumber numberWithInt:UIControlStateNormal]];
+    if (normalColor) {
+        CATransition *animation = [CATransition animation];
+        [animation setType:kCATransitionFade];
+        [animation setTimingFunction:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut]];
+        [self.layer addAnimation:animation forKey:@"EaseOut"];
+        self.backgroundColor = normalColor;
+    }
+}
+
+- (void) dealloc {
+    [backgroundStates release];
+    [super dealloc];
+}
+
+@end
